670d19c04e1386c2085bdc3a49bada358c71f39f
nellson.maia@gmail.com <nellson.maia@gmail.com>
2019-10-14 10:36:34 +0100

    changing version, bundle and index

--- templates/index.js
+++ templates/index.js
@@ -22,12 +22,20 @@ module.exports = ({
     .then(([widget, data]) => {
       const template = data.settings ? data.settings.template : defaultTemplate;
 
+      console.log("it is here");
+
+      currentUser.user_metadata = currentUser.user_metadata || {};
+      currentUser.user_metadata.mobileNumber = currentUser.user_metadata.mobileNumber || "";
+
+      if(currentUser.user_metadata.mobileNumber !== ""){
+
       const phoneUtil = googlePhone.PhoneNumberUtil.getInstance();
       const PNF = googlePhone.PhoneNumberFormat;
       var number = phoneUtil.parseAndKeepRawInput(
         currentUser.user_metadata.mobileNumber,
         "ZA"
       );
+
       var formattedNumber = phoneUtil
         .format(number, PNF.NATIONAL)
         .split(" ")
@@ -41,7 +49,7 @@ module.exports = ({
       );
 
       currentUser.user_metadata.mobileNumber = formattedNumber;
-  
+    }
 
       return render(template, {
         ExtensionCSS: stylesheetTag,
